7f106c51ed9fee421c7fa6f17bef6dbd0f1ce4af
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2023-01-18 17:50:50 -0800

    Adding an additional fallback for the description of the wiki command output

--- src/commands/wiki.ts
+++ src/commands/wiki.ts
@@ -19,7 +19,7 @@ module.exports = {
 		const title = document.getElementById('firstHeading').textContent
 		const url = document.querySelector('[title="View the content page [c]"]')?.getAttribute('href') ?? `/index.php?search=${userInput.replace(/\s/g, '+')}`
 		const imgURL = document.getElementsByTagName('img')[0].getAttribute('src')!
-		let description = document.getElementsByTagName('p')[0]?.textContent.trim() ?? 'No description available'
+		let description = document.getElementsByTagName('p')[0]?.textContent.trim() ?? document.getElementsByTagName('li')[0]?.textContent.trim() ?? 'No description available'
 		if (/Lore/.test(data)) description = data.match(/(?<=<b>Lore<\/b>: ).+?(?=\n)/s)!.toString()
 		else if (/Defense Statistics/.test(data)) description = document.querySelector('#mw-content-text > table.floatright > tr:nth-child(3) > td')!.textContent.trim()
 		const wikiEmbed = new MessageEmbed()
