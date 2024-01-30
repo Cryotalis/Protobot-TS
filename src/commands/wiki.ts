859329a78d6c7b24f0bef4f0720493ca35d13a7a
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2024-01-29 16:20:34 -0800

    Added better support for Hero Ability pages and fixed an issue with blank description for some pages

--- src/commands/wiki.ts
+++ src/commands/wiki.ts
@@ -11,22 +11,26 @@ module.exports = {
 	async execute(interaction: ChatInputCommandInteraction) {
 		const userInput = interaction.options.getString('search')!
 		let {data} = await axios.get(`https://wiki.dungeondefenders2.com/index.php?search=${userInput}`)
+		
 		if (!/View the/.test(data) && /data-serp-pos/.test(data)) {
-			({data} = await axios.get('https://wiki.dungeondefenders2.com/index.php?search=' + data.match(/(?<=data-serp-pos="1">).+?(?=<\/a>)/).toString()))
+			data = await axios.get('https://wiki.dungeondefenders2.com/index.php?search=' + data.match(/(?<=data-serp-pos="1">).+?(?=<\/a>)/).toString())
 		}
+		
 		const document = parse(data)
 		const title = document.getElementById('firstHeading')!.textContent
 		const url = document.querySelector('[title="View the content page [c]"]')?.getAttribute('href') ?? `/index.php?search=${userInput.replace(/\s/g, '+')}`
 		const imgURL = document.getElementsByTagName('img')[0].getAttribute('src')!
-		let description = document.getElementsByTagName('p')[0]?.textContent.trim() ?? document.getElementsByTagName('li')[0]?.textContent.trim() ?? 'No description available'
+		let description = document.getElementsByTagName('p')[0]?.textContent.trim() || document.getElementsByTagName('li')[0]?.textContent.trim() || 'No description available'
+		
 		if (/Lore/.test(data)) description = data.match(/(?<=<b>Lore<\/b>: ).+?(?=\n)/s)!.toString()
-		else if (/Defense Statistics/.test(data)) description = document.querySelector('#mw-content-text > table.floatright > tr:nth-child(3) > td')!.textContent.trim()
+		else if (/(Ability|Defense) Statistics/.test(data)) description = document.querySelector('#mw-content-text > table.floatright > tr:nth-child(3) > td')!.textContent.trim()
+
 		const wikiEmbed = new EmbedBuilder()
 			.setAuthor({name: 'Dungeon Defenders 2 Wiki', iconURL: 'https://i.imgur.com/ebPXRqR.png'})
 			.setTitle(title)
 			.setURL(`https://wiki.dungeondefenders2.com${url}`)
 			.setDescription(description.length > 350 ? `${description.substring(0, 350)}...` : description)
-			.setImage(imgURL.includes('mediawiki') ? '' : `https://wiki.dungeondefenders2.com${imgURL}`)
+			.setImage(imgURL.includes('mediawiki') ? null : `https://wiki.dungeondefenders2.com${imgURL}`)
 			.setThumbnail('https://wiki.dungeondefenders2.com/resources/assets/dd2_logo2.png?b4db7')
 			.setColor('Blue')
 
