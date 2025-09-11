4696d940f369ff2de6ec8198e7156f59e23c7b85
Hywell <elliottyh@gmail.com>
2025-09-11 09:39:46 -0700

    Code cleanup and reorganization

--- src/commands/drakenfrost.ts
+++ src/commands/drakenfrost.ts
@@ -6,50 +6,61 @@ export const command = {
 		.setDescription('Show the weapons and mods that are currently in rotation in Drakenfrost Keep')
 	,
 	async execute(interaction: ChatInputCommandInteraction) {
-		const nextDate = new Date()
-		const today = new Date()
-		const theDay = new Date(1554786000000) // April 9th, 2019, 5am UTC
+		// Drakenfrost Keep rotation changes on Tuesdays at 5AM (UTC)
+		const TIMESTAMP = 1554786000000 // April 9th, 2019, 5am UTC, used as a reference to calculate week number
+		const now = new Date()
+		now.setMinutes(now.getMinutes() + now.getTimezoneOffset()) // Ensure that UTC is being used
 		
-		today.setMinutes(today.getMinutes() + new Date().getTimezoneOffset()) // Ensure that UTC is being used
-		nextDate.setMinutes(nextDate.getMinutes() + new Date().getTimezoneOffset()) // Ensure that UTC is being used
-		nextDate.setDate(nextDate.getDate() + (9-nextDate.getDay())%7)
-		nextDate.setHours(5)
-		nextDate.setMinutes(0, 0, 0)
-
-		if (nextDate.valueOf() <= today.valueOf()) {nextDate.setDate(nextDate.getDate() + 7)} // Add a week if nextDate is in the past
-		let timeDiff = (nextDate.valueOf() - today.valueOf()) / 1000 // Time difference between next Monday at 5AM UTC and now in seconds
-		const week = Math.floor((today.valueOf() - theDay.valueOf()) / 8.64e+7 / 7 % 4) // 8.64e+7 = 1 day
-
-		//Calculates time difference
-		const diffDays = Math.floor(timeDiff / 86400)
-		timeDiff -= diffDays * 86400
-		const diffHours = Math.floor(timeDiff / 3600)
-		timeDiff -= diffHours * 3600
-		const diffMins = Math.floor(timeDiff / 60)
-		timeDiff -= diffMins * 60
-		const diffSecs = Math.floor(timeDiff)
+		const nextDate = new Date( // Gets the next Tuesday at 5AM (UTC)
+			now.getUTCFullYear(),
+			now.getUTCMonth(),
+			now.getUTCDate() + (9 - now.getUTCDay()) % 7,
+			5, 0, 0, 0
+		)
+		if (nextDate <= now) nextDate.setDate(nextDate.getDate() + 7)
 		
-		const Img = ['https://i.imgur.com/pMJ8J5X.png', 'https://i.imgur.com/r19VbPW.png', 'https://i.imgur.com/4mHTFMv.png', 'https://i.imgur.com/GXBplZd.png'][week]
-		const DFKMods = ['Torchbearer', 'Frozen Path', 'Frostfire Remnants', "Drakenlord's Soul"]
-		DFKMods[week] = `**${DFKMods[week]}**`
-		nextDate.setMinutes(nextDate.getMinutes() - new Date().getTimezoneOffset()) // Convert the UTC time back to local time
+		// Calculate time difference
+		// TODO: Time difference should be calculated with a util function
+		let timeDiff = (nextDate.getTime() - now.getTime()) / 1000
+		const dayDiff = Math.floor(timeDiff / 86400)
+		timeDiff -= dayDiff * 86400
+		const hourDiff = Math.floor(timeDiff / 3600)
+		timeDiff -= hourDiff * 3600
+		const minDiff = Math.floor(timeDiff / 60)
+		timeDiff -= minDiff * 60
+		const secDiff = Math.floor(timeDiff)
+		
+		nextDate.setMinutes(nextDate.getMinutes() - nextDate.getTimezoneOffset()) // Convert back to local time
+		
+		const weekNum = Math.floor((now.getTime() - TIMESTAMP) / 8.64e+7 / 7 % 4) // 8.64e+7 = 1 day
+		const rotationImages = [
+			'https://i.imgur.com/pMJ8J5X.png',
+			'https://i.imgur.com/r19VbPW.png',
+			'https://i.imgur.com/4mHTFMv.png',
+			'https://i.imgur.com/GXBplZd.png'
+		]
+		const DFKMods = ['Torchbearer', 'Frozen Path', 'Frostfire Remnants', 'Drakenlord\'s Soul']
+		DFKMods[weekNum] = `**${DFKMods[weekNum]}**`
 
 		const DFKEmbed = new EmbedBuilder()
 			.setColor('Blue')
 			.setTitle('__**Time until next rotation:**__')
 			.setThumbnail('https://i.imgur.com/BrTSxJu.png')
 			.addFields([
-				{name: '\u200B    ' + diffDays + '           ' + diffHours + '            ' + diffMins + '             ' + diffSecs, value: 'Days \u2009 Hours \u2009 Minutes \u2009 Seconds\n\u200B '},
-				{name: 'Week 1', value: DFKMods[0], inline: true},
-				{name: 'Week 2', value: DFKMods[1], inline: true},
-				{name: '\u200b', value: '\u200b', inline: true},
-				{name: 'Week 3', value: DFKMods[2], inline: true},
-				{name: 'Week 4', value: DFKMods[3], inline: true},
-				{name: '\u200b', value: `\u200b\n\u200b`, inline: true},
-				{name: '**Next Rotation At**:', value: `<t:${nextDate.getTime()/1000}:F>`}
+				{
+					name: `\u200B    ${dayDiff}           ${hourDiff}            ${minDiff}             ${secDiff}`,
+					value: 'Days \u2009 Hours \u2009 Minutes \u2009 Seconds\n\u200B '
+				},
+				{ name: 'Week 1', value: DFKMods[0], 	   inline: true },
+				{ name: 'Week 2', value: DFKMods[1], 	   inline: true },
+				{ name: '\u200b', value: '\u200b', 		   inline: true },
+				{ name: 'Week 3', value: DFKMods[2], 	   inline: true },
+				{ name: 'Week 4', value: DFKMods[3], 	   inline: true },
+				{ name: '\u200b', value: `\u200b\n\u200b`, inline: true },
+				{ name: '**Next Rotation At**:', value: `<t:${nextDate.getTime()/1000}:F>`}
 			])
-			.setImage(Img)
+			.setImage(rotationImages[weekNum])
 
-		await interaction.reply({embeds: [DFKEmbed]})
+		interaction.reply({ embeds: [DFKEmbed] })
 	}
 }
