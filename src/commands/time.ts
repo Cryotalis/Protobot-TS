d6a61bcfdd3a97de9a57bbca4e5dc163c1e14d2f
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2025-09-16 23:54:38 -0700

    Removed the ability to convert between timezones from the time command

--- src/commands/time.ts
+++ src/commands/time.ts
@@ -1,24 +1,19 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
-import { findLastWord } from '../utils/string.js'
-import { dateToString, findTimeZone, dateStringToUnix } from '../utils/time.js'
 
 export const command = {
 	data: new SlashCommandBuilder()
 		.setName('time')
 		.setDescription('Display the current time in Gainesville, Florida')
-		.addStringOption(option => option.setName('time').setDescription("The date/time you'd like to convert from"))
-		.addStringOption(option => option.setName('timezone').setDescription("The timezone you'd like to convert to"))
 	,
 	async execute(interaction: ChatInputCommandInteraction) {
-		const timeInput = interaction.options.getString('time')!
-		if (!timeInput) return await interaction.reply(`It is currently \`${dateToString(new Date(), 'ET')}\` in Gainesville.`)
-		
-		const timeZone1 = findTimeZone(findLastWord(timeInput)).name
-		const timeZone2 = interaction.options.getString('timezone')
-		const unix = dateStringToUnix(timeInput)!
-		const time1 = `\`${dateToString(new Date(unix), timeZone1, true)}\``
-		const time2 = timeZone2 ? `\`${dateToString(new Date(unix), timeZone2, true)}\`` : `<t:${unix/1000}>`
+		const now = new Date()
+        const options: Intl.DateTimeFormatOptions = {
+            dateStyle: 'full',
+            timeStyle: 'medium',
+            timeZone: 'America/New_York',
+        }
+        const gainesvilleDateTime = new Intl.DateTimeFormat('en-US', options).format(now)
 
-		await interaction.reply(`${time1} is ${time2}`)
-	}
+        interaction.reply(`It is currently \`${gainesvilleDateTime}\` in Gainesville, Florida.`)
+    }
 }
