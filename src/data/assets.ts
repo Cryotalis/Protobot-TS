5949337ebda46138efc42b03297eac328a9fb43d
Cryotalis <31523573+Cryotalis@users.noreply.github.com>
2025-09-23 23:19:47 -0700

    Reorganizing assets

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -1,12 +1,12 @@
 import { AttachmentBuilder } from 'discord.js'
 
-export const rumbleIcon = new AttachmentBuilder('./assets/Elemental_Rumble_Icon.png', {
+export const rumbleIcon = new AttachmentBuilder('./assets/attachments/Elemental_Rumble_Icon.png', {
     name: 'Elemental_Rumble_Icon.png'
 })
 
 export const rumbleRotationImages = [
-    new AttachmentBuilder('./assets/Fire_Week_Items.png' , { name: 'Fire_Week_Items.png'  }),
-    new AttachmentBuilder('./assets/Water_Week_Items.png', { name: 'Water_Week_Items.png' }),
-    new AttachmentBuilder('./assets/Storm_Week_Items.png', { name: 'Storm_Week_Items.png' }),
-    new AttachmentBuilder('./assets/Earth_Week_Items.png', { name: 'Earth_Week_Items.png' }),
+    new AttachmentBuilder('./assets/attachments/Fire_Week_Items.png' , { name: 'Fire_Week_Items.png'  }),
+    new AttachmentBuilder('./assets/attachments/Water_Week_Items.png', { name: 'Water_Week_Items.png' }),
+    new AttachmentBuilder('./assets/attachments/Storm_Week_Items.png', { name: 'Storm_Week_Items.png' }),
+    new AttachmentBuilder('./assets/attachments/Earth_Week_Items.png', { name: 'Earth_Week_Items.png' }),
 ]
